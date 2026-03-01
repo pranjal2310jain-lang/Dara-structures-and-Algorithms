f5243aa645249eb1e29fd0a85be84f6a00e1962c
pranjal2310jain-lang <pranjal2310jain@gmail.com>
2026-03-02 00:56:50 +0530

    Create 01_Two_Sum_II.cpp
    Add Two Sum II using Two Pointers

---
+++ two pointers/01_Two_Sum_II.cpp
@@ -0,0 +1,18 @@
+// Problem: Two Sum II
+// Pattern: Two Pointers
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i = 0, j = numbers.size() - 1;
+        while(i < j){
+            int sum = numbers[i] + numbers[j];
+            if(sum == target) return {i+1, j+1};
+            else if(sum > target) j--;
+            else i++;
+        }
+        return {};
+    }
+};
